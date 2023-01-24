4ec19e3a79001c02b8978a8fad1ec7607cbac56b
13JaviG <13javiergc@gmail.com>
2023-01-24 14:12:03 +0100

    correcciones

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,10 +1,10 @@
 import { Square } from "./Square"
 
-export function Board({ updateBoard, board }) {
+export function Board({ board, updateBoard}) {
     return (
         <section className='game'>
             {
-                board.map((_, index) => {
+                Array.from(board).map((_, index) => {
                     return (
                         <Square key={index} index={index} updateBoard={updateBoard}>
                             {board[index]}
